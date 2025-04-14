802bd74b9db64f8543effd2f542a812bacd33526
wolve324 <111057273+wolve324@users.noreply.github.com>
2025-04-14 15:38:48 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './statics.css'; // Your CSS file
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
